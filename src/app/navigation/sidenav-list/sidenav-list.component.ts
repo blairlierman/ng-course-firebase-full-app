ad8cc990e4f49c14d6fa99758b3e66235cb00a21
Blair Lierman <blairlierman@gmail.com>
2021-04-12 21:07:31 -0700

    Header + Sidenav List to separate components

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavListComponent implements OnInit {
 
+  close = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClose() {
+    this.close.emit();
   }
 
 }
