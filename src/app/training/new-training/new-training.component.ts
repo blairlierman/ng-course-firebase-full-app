e6bb8bbc9b2006155e858395b8fd662098bd5ff8
Blair Lierman <blairlierman@gmail.com>
2021-04-22 18:41:12 -0700

    Load Exercises from Firebase Firestore. Add AngularFire to project.

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
@@ -9,12 +11,13 @@ import { TrainingService } from '../training.service';
   styleUrls: ['./new-training.component.scss']
 })
 export class NewTrainingComponent implements OnInit {
+  
+  exercises: Observable<any> = new Observable<any>();
 
-  constructor(private trainingService: TrainingService) { }
-  exercises: Exercise[] = [];
+  constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit(): void {
-    this.exercises = this.trainingService.getAvailableExercises();
+    this.exercises = this.db.collection("availableExercises").valueChanges();
   }
 
   onStartTraining(form: NgForm)
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD09iBZk7YSHvM3rYk5_QBPtmipMMmfprc",
+    authDomain: "ng-fitness-tracker-3525b.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-3525b.firebaseio.com",
+    projectId: "ng-fitness-tracker-3525b",
+    storageBucket: "ng-fitness-tracker-3525b.appspot.com",
+    messagingSenderId: "48026182579",
+    appId: "1:48026182579:web:a80cf37e9d96fc1142a1c6",
+  }
 };
 
 /*
