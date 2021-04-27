d885cc201e12f7b329e087d9ccf7395be1f60bd2
Blair Lierman <blairlierman@gmail.com>
2021-04-26 20:52:30 -0700

    Map available exercises with snapshot changes. This includes the id field that we can map in.

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -4,6 +4,7 @@ import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-new-training',
@@ -17,7 +18,18 @@ export class NewTrainingComponent implements OnInit {
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit(): void {
-    this.exercises = this.db.collection("availableExercises").valueChanges();
+    this.exercises = 
+      this.db.collection("availableExercises").snapshotChanges()
+      .pipe(
+        map(docArray => {
+          return docArray.map(doc => {
+            return {
+              id: doc.payload.doc.id,
+              ...doc.payload.doc.data() as {}
+            } 
+          })
+        })
+      )
   }
 
   onStartTraining(form: NgForm)
