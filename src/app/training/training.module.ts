2d8c00f2dbcb3b5be57defcad130fadc0342bac1
Blair Lierman <blairl@outlook.com>
2021-06-06 18:23:21 -0700

    Move Common Module to Shared Module

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MaterialModule } from "../material.module";
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { ReactiveFormsModule } from "@angular/forms";
 import { CurrentTrainingComponent } from "./current-training/current-training.component";
 import { NewTrainingComponent } from "./new-training/new-training.component";
 import { PastTrainingComponent } from "./past-training/past-training.component";
 import { TrainingComponent } from "./training.component";
 import { StopTrainingComponent } from "./current-training/stop-training.component";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [    
@@ -18,11 +16,8 @@ import { StopTrainingComponent } from "./current-training/stop-training.componen
         StopTrainingComponent,       
     ],
     imports: [
-        CommonModule,
-        FormsModule,
         ReactiveFormsModule,
-        MaterialModule,
-        FlexLayoutModule
+        SharedModule
     ],
     exports: [],
     entryComponents: [StopTrainingComponent]
