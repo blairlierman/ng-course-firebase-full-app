9b97cb65456385274346fda44b7832d5a9be968a
Blair Lierman <blairl@outlook.com>
2021-06-06 17:44:06 -0700

    Run ESLint on Auth Guard file

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,20 +1,17 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";     
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService, private router: Router)
-    {}
-    
+    constructor(private authService: AuthService, private router: Router) { }
+
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        if (this.authService.isAuth())
-        {
+        if (this.authService.isAuth()) {
             return true;
         }
-        else
-        {
+        else {
             this.router.navigate(['/login'])
             return false;
         }
