b3a1f3e6c6a52f16dfaf39e8d0d96a1c2b568288
Blair Lierman <blairlierman@gmail.com>
2021-10-16 15:35:30 -0700

    Remove Exercises and Loading Subscriptions

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,15 +1,10 @@
 import { EventEmitter, Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Subject } from "rxjs";
 
 @Injectable()
 export class UIService {
-    // loadingStateChange: Subject<boolean>;
-    exercisesLoadingStateChanged: Subject<boolean>;
 
     constructor(private snackbar: MatSnackBar) {
-        // this.loadingStateChange = new Subject();
-        this.exercisesLoadingStateChanged = new Subject();
     }
 
     showSnackbar(message: string, action: string | undefined, duration: number)
