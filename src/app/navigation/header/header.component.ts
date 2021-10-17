994bc97bf095411f7eba90377188c8b149448145
Blair Lierman <blairlierman@gmail.com>
2021-10-17 11:57:08 -0700

    Change Header Component to use NgRx Store

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,28 +1,24 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
+import * as fromRoot from "../../app.reducer"
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
 
   @Output() sidenavToggle = new EventEmitter();
-  isAuth: boolean = false;
+  isAuth$: Observable<boolean> | undefined;
   authSubscription! : Subscription;
 
-  constructor(private authService: AuthService) { }
-
-  ngOnDestroy(): void {
-    this.authSubscription.unsubscribe();
-  }
+  constructor(private authService: AuthService, private store: Store<fromRoot.State>) { }
 
   ngOnInit(): void {
-    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
-        this.isAuth = authStatus;
-    })
+    this.isAuth$ = this.store.select(fromRoot.getIsAuth)
   }
 
   onToggleSidenav() {
