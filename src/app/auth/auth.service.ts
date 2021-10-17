b15f78f1ec90b5513ce582915b03713057a61492
Blair Lierman <blairlierman@gmail.com>
2021-10-17 11:45:07 -0700

    Add new Auth Reducer (WIP).

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,11 +8,10 @@ import { UIService } from "../shared/ui.service";
 import { Store } from "@ngrx/store";
 import * as fromRoot from '../app.reducer'
 import * as UI from '../shared/ui.actions'
+import * as Auth from '../auth/auth.actions'
 
 @Injectable()
 export class AuthService {
-    authChange = new Subject<boolean>();
-    private isAuthenticated: boolean = false;
 
     constructor(private router: Router, private afAuth: AngularFireAuth,
         private trainingService: TrainingService,
@@ -22,14 +21,12 @@ export class AuthService {
     initAuthListener() {
         this.afAuth.authState.subscribe(user => {
             if(user) {
-                this.isAuthenticated = true;
-                this.authChange.next(true);
+              this.store.dispatch(new Auth.SetAuthenticated());
                 this.router.navigate(['/training']);
             }
             else {
                 this.trainingService.cancelSubscriptions();
-                this.isAuthenticated = false;
-                this.authChange.next(false);
+                this.store.dispatch(new Auth.SetUnauthenticated());
                 this.router.navigate(['/login']);
             }
         });
@@ -62,9 +59,5 @@ export class AuthService {
 
     logout() {
         this.afAuth.signOut();
-    }
-
-    isAuth() {
-        return this.isAuthenticated;
     }
 }
