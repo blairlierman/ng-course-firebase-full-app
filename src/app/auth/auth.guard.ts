297b3ee2948c789fbfda488d146604797f9a1cff
Blair Lierman <blairlierman@gmail.com>
2021-10-17 11:56:54 -0700

    Change AuthGuard to use NgRx Store

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,28 +2,19 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
+import * as fromRoot from "../app.reducer"
+import { Store } from "@ngrx/store";
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanLoad {
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private authService: AuthService, private router: Router,
+      private store: Store<fromRoot.State>) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        if (this.authService.isAuth()) {
-            return true;
-        }
-        else {
-            this.router.navigate(['/login'])
-            return false;
-        }
+      return this.store.select(fromRoot.getIsAuth);
     }
 
     canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        if (this.authService.isAuth()) {
-            return true;
-        }
-        else {
-            this.router.navigate(['/login'])
-            return false;
-        }
+      return this.store.select(fromRoot.getIsAuth);
     }
 }
